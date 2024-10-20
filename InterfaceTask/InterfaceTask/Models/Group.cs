9cc98656b8a91199a5555238dcec9025d2530cf3
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-20 13:03:18 +0400

    task 2 partly added

--- InterfaceTask/InterfaceTask/Models/Group.cs
+++ InterfaceTask/InterfaceTask/Models/Group.cs
@@ -3,11 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace InterfaceTask.Models
 {
     internal class Group
     {
+        static string[] Groups = new string[0];
+        static int GroupCount=0;
+        public int GroupId;
+        public string GroupName { get; set; }
+        public string[] Students = new string[0];
+        int StudentCount = 0;
+        int StudentId = 0;
+   
 
+        public Group(string groupName)
+        {
+
+            GroupId = ++GroupCount;
+            GroupName = groupName;
+
+
+
+            Array.Resize(ref Groups, Groups.Length + 1);
+            Groups[Groups.Length - 1] = groupName;
+
+        }
+
+        
+        public void AddStudent(string student)
+        {
+            
+            Array.Resize(ref Students, Students.Length + 1);
+            Students[Students.Length - 1] = student;
+            StudentCount++;
+            StudentId = StudentCount;
+   
+        }
+        public  void GetGroupInfo()
+        {
+            Console.WriteLine($"Group's Name: {GroupName}, attendance: {StudentCount}");
+
+            
+        }
+        public void GetId()
+        {
+            Console.WriteLine(StudentId);
+        }
+       
     }
 }
--- InterfaceTask/InterfaceTask/Program.cs
+++ InterfaceTask/InterfaceTask/Program.cs
@@ -6,15 +6,24 @@ namespace InterfaceTask
     {
         static void Main(string[] args)
         {
-            
-            
-            Student student = new("Ayxan", "Mammadli");
-            Student student2 = new("Ayxan", "Mammadli");
-            Student student3 = new("Ayxan", "Mammadli");
-            Student student4= new("Ayxan", "Mammadli");
-            Student student5= new("Ayxan", "Mammadli");
-            Student student6= new("Ayxan", "Mammadli");
-            Student student7= new("Ayxan", "Mammadli");
+
+
+            //Student student = new("Ayxan", "Mammadli");
+            //Student student2 = new("Ayxan", "Mammadli");
+            //Student student3 = new("Ayxan", "Mammadli");
+            //Student student4 = new("Ayxan", "Mammadli");
+            //Student student5 = new("Ayxan", "Mammadli");
+            //Student student6 = new("Ayxan", "Mammadli");
+            //Student student7 = new("Ayxan", "Mammadli");
+            //Console.WriteLine(student7.Id);
+
+            Group group = new("Bp-217");
+            group.AddStudent("ayxan memmedli");
+            group.AddStudent("ayxan memmedli");
+
+
+            group.GetGroupInfo();
+
 
 
 
